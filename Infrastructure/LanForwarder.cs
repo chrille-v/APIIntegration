736d925c1d3c2bd35c165d75494d3d5aca3b2f1a
Antun Rukavina <antunrukavina8@gmail.com>
2025-12-02 15:13:33 +0100

    Implement LAN forwarding logic (endpoint resolution, POST request, logging, error handling)

--- Infrastructure/LanForwarder.cs
+++ Infrastructure/LanForwarder.cs
@@ -4,6 +4,7 @@ using APIIntegration.Core.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Json;
+using System.Text;
 
 public class LanForwarder : ILanForwarder
 {
@@ -18,9 +19,43 @@ public class LanForwarder : ILanForwarder
         _logger = logger;
     }
 
-    public Task<bool> ForwardJobAsync(Message message, CancellationToken cancellationToken)
+
+    //depends on message type(update or status) to resolve endpoint
+    private string ResolveEndpoint(Message message)
     {
-        throw new NotImplementedException();
+        return message.Type switch
+        {
+            MessageType.JobUpdate => _lanSettings.JobUpdateEndpoint,
+            MessageType.JobStatus => _lanSettings.JobStatusEndpoint,
+            _ => throw new ArgumentOutOfRangeException(nameof(message.Type))
+        };
+    }
+
+    public async Task<bool> ForwardJobAsync(Message message, CancellationToken cancellationToken)
+    {
+        var endpoint = ResolveEndpoint(message);
+        var url = $"{_lanSettings.BaseUrl}{endpoint}";
+
+        var content = new StringContent(message.Payload, Encoding.UTF8, "application/json");
+
+        try
+        {
+            var response = await _httpClient.PostAsync(url, content, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogWarning("LAN forward failed: {Status} - MessageId={MessageId}",response.StatusCode, message.MessageId);
+                return false;
+            }
+
+            _logger.LogInformation("Lan forward successful. MessageId={MessageId}", message.MessageId);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error forwarding message to LAN. MessageId={MessageId}", message.MessageId);
+            return false;
+        }
     }
 }
 
